d775c033b74e689f1e22e0af9f2383301b72236b
dtjanaka <15240801+dtjanaka@users.noreply.github.com>
2020-09-21 23:21:54 -0700

    Proper body.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -192,6 +192,15 @@ async function submitComment() {
 
     const response = await fetch('/comments', {
       method: 'POST',
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0',
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Upgrade-Insecure-Requests': '1',
+      },
       body:
         'comment=' +
         commentForm.get('comment') +
