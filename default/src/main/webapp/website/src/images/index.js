32f56670b1e291e31a5b7c6c81b061ca7bc07ea2
dtjanaka <15240801+dtjanaka@users.noreply.github.com>
2020-10-18 19:19:59 -0700

    Working on footer.

--- default/src/main/webapp/website/src/images/index.js
+++ default/src/main/webapp/website/src/images/index.js
@@ -1,6 +1,8 @@
+export const logo = require('./logo.svg');
+export const logo2 = require('./logo_2.svg');
 export const fb = require('./socials/fb.svg');
 export const ig = require('./socials/ig.svg');
 export const li = require('./socials/li.svg');
 export const gh = require('./socials/gh.svg');
 export const yt = require('./socials/yt.svg');
-export const logo = require('./logo.svg');
+export const email = require('./email.svg');
